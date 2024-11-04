92a8f5cf2909fc1d1d8feb46f16f353139655edc
NguyenVuThe <nguyenvtk2110@gmail.com>
2024-11-04 22:13:31 +0700

    Add No Favourite Fragment

---
+++ app/src/main/java/com/app/java/trackingrunningapp/ui/favorite/NoFavouriteFragment.kt
@@ -0,0 +1,19 @@
+package com.app.java.trackingrunningapp.ui.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.app.java.trackingrunningapp.R
+
+class NoFavouriteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nofavourite, container, false)
+    }
+}
