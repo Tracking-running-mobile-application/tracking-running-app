256b08c4668edf23d915a1ffd2d2b64f6bc71982
thaison1939 <70836333+thaison1939@users.noreply.github.com>
2024-11-05 20:40:51 +0700

    fix minor bugs

---
+++ app/src/main/java/com/app/java/trackingrunningapp/ui/runPage/MetricItemAdapter.kt
@@ -0,0 +1,32 @@
+package com.app.java.trackingrunningapp.ui.runPage
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.app.java.trackingrunningapp.R
+import com.app.java.trackingrunningapp.ui.runPage.MetricItemAdapter.MetricViewHolder
+
+class MetricItemAdapter(private val MetricItems: List<MetricItem>) :
+    RecyclerView.Adapter<MetricViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MetricViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_run_metric, parent, false)
+        return MetricViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MetricViewHolder, position: Int) {
+        val item = MetricItems[position]
+        holder.labelTextView.text = item.label
+        holder.valueTextView.text = item.value
+    }
+
+    override fun getItemCount(): Int {
+        return MetricItems.size
+    }
+
+    class MetricViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var labelTextView: TextView = itemView.findViewById(R.id.labelTextView)
+        var valueTextView: TextView = itemView.findViewById(R.id.valueTextView)
+    }
+}
