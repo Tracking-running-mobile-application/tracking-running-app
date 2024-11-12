0817cb1cbd76789b7c4edc3a6d4703b58a5ce2e9
Ngô Quý Đăng Tuấn <dangtuannq@gmail.com>
2024-11-12 14:42:23 +0700

    Add more item on list

--- app/src/main/java/com/app/java/trackingrunningapp/ui/newfavourite/FavouriteRuns.kt
+++ app/src/main/java/com/app/java/trackingrunningapp/ui/newfavourite/FavouriteRuns.kt
@@ -24,6 +24,24 @@ class FavouriteRuns : Fragment() {
         val runList = mutableListOf(
             RunItem("Normal Run", "12KM", "22/10/2024"),
             RunItem("Normal Run", "12KM", "2/9/2024"),
+            RunItem("Morning Run", "8KM", "1/8/2024"),
+            RunItem("Evening Sprint", "5KM", "15/7/2024"),
+            RunItem("Marathon Prep", "20KM", "10/7/2024"),
+            RunItem("Trail Adventure", "15KM", "5/6/2024"),
+            RunItem("Casual Jog", "6KM", "1/5/2024"),
+            RunItem("City Run", "10KM", "25/4/2024"),
+            RunItem("Weekend Dash", "7KM", "20/4/2024"),
+            RunItem("Quick Run", "3KM", "18/4/2024"),
+            RunItem("Challenge Run", "13KM", "15/4/2024"),
+            RunItem("Sunset Run", "9KM", "10/4/2024"),
+            RunItem("Morning Run", "8KM", "1/8/2024"),
+            RunItem("Trail Adventure", "14KM", "22/3/2024"),
+            RunItem("Hill Run", "10KM", "10/3/2024"),
+            RunItem("Fitness Run", "11KM", "1/3/2024"),
+            RunItem("Fun Run", "5KM", "20/2/2024"),
+            RunItem("Marathon Race", "42KM", "15/2/2024"),
+            RunItem("Light Jog", "4KM", "5/2/2024"),
+            RunItem("Intense Sprint", "2KM", "25/1/2024")
         )
 
         val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
