b832a1a13d06e20b90714e5ea6a5632eae23dd07
Ngô Quý Đăng Tuấn <dangtuannq@gmail.com>
2024-11-18 13:08:50 +0700

    Modify items

--- app/src/main/java/com/app/java/trackingrunningapp/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/app/java/trackingrunningapp/ui/setting/SettingFragment.kt
@@ -24,14 +24,13 @@ class SettingFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_setting, container, false)
 
         spinner1 = view.findViewById(R.id.theme)
-        var themeItems = listOf("Light Theme", "Dark Theme", "Custom Theme", "High Contrast Theme", "Material Design Theme", "Flat Design Theme", "Syntax Highlighting Themes", "IDE Themes", "Debugger Themes", "Feature-Based Themes", "Module-Specific Themes", "Mobile vs. Desktop Themes", "E-commerce Theme", "Gaming Theme", "Health and Fitness Theme", "Educational Theme")
+        var themeItems = listOf("Light mode", "Dark mode")
         var themeAdapter = ArrayAdapter(requireContext(),R.layout.item_setting_theme,themeItems)
         themeAdapter.setDropDownViewResource(R.layout.item_setting_theme)
         spinner1.adapter = themeAdapter
         spinner1.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
                 val selectedItem = parent.getItemAtPosition(position) as String
-                Toast.makeText(requireContext(), "Selected: $selectedItem", Toast.LENGTH_SHORT).show()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
@@ -39,14 +38,13 @@ class SettingFragment : Fragment() {
         })
 
         spinner2 = view.findViewById(R.id.unit)
-        val unitItems = listOf("Millimeter (mm)", "Centimeter (cm)", "Meter (m)", "Kilometer (km)", "Inch (in)", "Foot (ft)", "Yard (yd)", "Mile (mi)", "Nautical Mile (nmi)")
+        val unitItems = listOf("Minutes per mile","Minutes per kilometer (Pace)")
         var unitAdapter = ArrayAdapter(requireContext(),R.layout.item_setting_unit,unitItems)
         unitAdapter.setDropDownViewResource(R.layout.item_setting_unit)
         spinner2.adapter = unitAdapter
         spinner2.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
                 val selectedItem = parent.getItemAtPosition(position) as String
-                Toast.makeText(requireContext(), "Selected: $selectedItem", Toast.LENGTH_SHORT).show()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
@@ -54,14 +52,13 @@ class SettingFragment : Fragment() {
         })
 
         spinner3 = view.findViewById(R.id.language)
-        var languageItems = listOf("English", "German", "French", "Spanish", "Italian", "Japanese", "Chinese", "Korean", "Portuguese", "Russian")
+        var languageItems = listOf("English", "Vietnamese")
         var languageAdapter = ArrayAdapter(requireContext(),R.layout.item_setting_language,languageItems)
         languageAdapter.setDropDownViewResource(R.layout.item_setting_language)
         spinner3.adapter = languageAdapter
         spinner3.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
                 val selectedItem = parent.getItemAtPosition(position) as String
-                Toast.makeText(requireContext(), "Selected: $selectedItem", Toast.LENGTH_SHORT).show()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
