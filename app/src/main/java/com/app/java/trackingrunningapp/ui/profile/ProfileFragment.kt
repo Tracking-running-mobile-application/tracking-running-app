f7dbca469dfcafed42cb514c8453b46b10ae9f3c
unknown <thangkay3009@gmail.com>
2024-11-24 23:20:13 +0700

    add profile page

--- app/src/main/java/com/app/java/trackingrunningapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/app/java/trackingrunningapp/ui/profile/ProfileFragment.kt
@@ -9,17 +9,37 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.app.java.trackingrunningapp.R
 import com.app.java.trackingrunningapp.databinding.FragmentProfileBinding
+import com.db.williamchart.view.BarChartView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.tabs.TabLayout
 
 class ProfileFragment : Fragment() {
-    private lateinit var binding:FragmentProfileBinding
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentProfileBinding.inflate(inflater,container,false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val barSet = listOf(
+            "Week 1" to 4F,
+            "Week 2" to 2F,
+            "Week 3" to 4.5F,
+            "Week 4" to 3F,
+            "Week 5" to 9F,
+            "Week 6" to 13F,
+        )
+        val barChart = view.findViewById<BarChartView>(R.id.barchart)
+        barChart.animate(barSet)
+        barChart.apply {
+            animation.duration = 1000L
+            labelsFormatter = { it.toInt().toString() }
+        }
+    }
 }
+
