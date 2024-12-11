1c6ce0ed2991bd6df1b09fb16d6651c0c980fccc
unknown <thangkay3009@gmail.com>
2024-12-11 08:40:31 +0700

    fix map rendering

--- app/src/main/java/com/app/java/trackingrunningapp/ui/run/RunFragment.kt
+++ app/src/main/java/com/app/java/trackingrunningapp/ui/run/RunFragment.kt
@@ -1,40 +1,40 @@
-package com.app.java.trackingrunningapp.ui.run_page
+package com.app.java.trackingrunningapp.ui.run
 
 import android.Manifest
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.app.java.trackingrunningapp.R
-import com.app.java.trackingrunningapp.ui.FusedLocationAPI.DefaultLocationClient
-import com.app.java.trackingrunningapp.ui.FusedLocationAPI.LocationService
+import com.app.java.trackingrunningapp.databinding.FragmentRunBinding
+import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
-import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
 import com.mapbox.maps.plugin.locationcomponent.location
+import com.mapbox.maps.plugin.viewport.viewport
 
 class RunFragment : Fragment() {
     private lateinit var binding: FragmentRunBinding
-    private var locationClient: DefaultLocationClient? = null
-    private var isOverlayVisible = true
-    private var isTracking = false
     private lateinit var mapView: MapView
     private val requestPermissionsLauncher =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
-            val allGranted = permissions.all { it.value }
+            val allGranted = permissions.all {
+                it.value
+            }
             if (allGranted) {
-                initializeMapAndLocation()
+                initMapAndLocation()
             } else {
-                Toast.makeText(requireContext(), "Permissions are required to proceed.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    requireContext(),
+                    "Permissions are required to proceed.",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
@@ -43,107 +43,99 @@ class RunFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentRunBinding.inflate(inflater,container,false)
+        binding = FragmentRunBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    @SuppressLint("InlinedApi")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mapView = view.findViewById(R.id.mapView)
+        initArrowAction()
+        mapView = binding.mapView
 
         val permissions = arrayOf(
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.POST_NOTIFICATIONS
         )
-
         // Check if all permissions are granted
-        if (permissions.all {
-                ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+        if (permissions.all {// false if 1 permission is denied
+                ContextCompat
+                    .checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
             }) {
-            initializeMapAndLocation()
+            initMapAndLocation()
         } else {
             // Request missing permissions
             requestPermissionsLauncher.launch(permissions)
         }
 
+    }
 
-        val toggleTrackingButton: Button = view.findViewById(R.id.toggleTrackingButton)
-        toggleTrackingButton.text = "Start Tracking" // Initial text
-
-        toggleTrackingButton.setOnClickListener {
-            if (isTracking) {
-                stopTracking()
-                toggleTrackingButton.text = "Start Tracking"
-            } else {
-                startTracking()
-                toggleTrackingButton.text = "Stop Tracking"
-            }
-            isTracking = !isTracking
+    private fun initArrowAction() {
+        binding.icArrowUp.setOnClickListener {
+            binding.icArrowDown.visibility = View.VISIBLE
+            binding.icArrowUp.visibility = View.GONE
+            binding.layoutMetric.root.visibility = View.GONE
         }
-
-        val toggleButton = view.findViewById<ImageView>(R.id.toggleButton)
-        val metricsRecyclerView = view.findViewById<RecyclerView>(R.id.metricsRecyclerView)
-        metricsRecyclerView.visibility = View.VISIBLE
-
-        toggleButton.setOnClickListener { v: View? ->
-            if (isOverlayVisible) {
-                metricsRecyclerView.visibility = View.GONE
-                toggleButton.rotation = 180f
-            } else {
-                metricsRecyclerView.visibility = View.VISIBLE
-                toggleButton.rotation = 0f
-            }
-            isOverlayVisible = !isOverlayVisible
+        binding.icArrowDown.setOnClickListener {
+            binding.icArrowUp.visibility = View.VISIBLE
+            binding.icArrowDown.visibility = View.GONE
+            binding.layoutMetric.root.visibility = View.VISIBLE
         }
+    }
 
-        val metricItems: MutableList<MetricItem> = ArrayList()
-        metricItems.add(MetricItem("Duration", "01:25:40"))
-        metricItems.add(MetricItem("Distance", "10.02 km"))
-        metricItems.add(MetricItem("Pace", "9:12 / km"))
-        metricItems.add(MetricItem("Calories", "220 kcal"))
+    private fun initMapAndLocation() {
+        /*  mapView.mapboxMap.loadStyle(Style.STANDARD) { style ->
+               mapView.location.updateSettings {
+                   enabled = true
+               }
+               val positionChangedListener = OnIndicatorPositionChangedListener { point ->
+                   val cameraOptions = CameraOptions.Builder()
+                       .center(point)
+                       .zoom(15.0)
+                       .build()
+                   mapView.mapboxMap.setCamera(cameraOptions)
+               }
+               mapView.location.addOnIndicatorPositionChangedListener(positionChangedListener)
+           }
 
-        metricsRecyclerView.layoutManager = GridLayoutManager(getContext(), 2)
-        val adapter = MetricItemAdapter(metricItems)
-        metricsRecyclerView.adapter = adapter
+         */
+        initMap()
 
     }
 
-    private fun initializeMapAndLocation() {
-        mapView.mapboxMap.loadStyle(Style.STANDARD) { style ->
-            mapView.location.updateSettings {
+    private fun initMap() {
+//        mapView = MapView(requireContext())
+        mapView.mapboxMap.loadStyle(Style.MAPBOX_STREETS) {
+//            setCamera(
+//                CameraOptions.Builder()
+//                    .center(Point.fromLngLat(21.0481, 105.80125))
+//                    .pitch(45.0)
+//                    .zoom(15.0)
+//                    .build()
+//            )
+            val locationPlugin = mapView.location
+            locationPlugin.updateSettings {
                 enabled = true
             }
-
-            val positionChangedListener = OnIndicatorPositionChangedListener { point ->
-                val cameraOptions = CameraOptions.Builder()
-                    .center(point)
-                    .zoom(15.0)
-                    .build()
-                mapView.mapboxMap.setCamera(cameraOptions)
+            locationPlugin.addOnIndicatorPositionChangedListener { point ->
+                // Khi vị trí thay đổi, di chuyển camera đến vị trí hiện tại
+                moveCameraToLocation(point)
             }
-
-            mapView.location.addOnIndicatorPositionChangedListener(positionChangedListener)
         }
     }
 
-    private fun startTracking() {
-        val context = requireContext()
-        val intent = Intent(context, LocationService::class.java).apply {
-            action = LocationService.ACTION_START
-        }
-        context.startService(intent)
-    }
-
-    private fun stopTracking() {
-        val context = requireContext()
-        val intent = Intent(context, LocationService::class.java).apply {
-            action = LocationService.ACTION_STOP
-        }
-        context.startService(intent)
+    private fun moveCameraToLocation(location: Point) {
+        mapView.mapboxMap.setCamera(
+            CameraOptions.Builder()
+                .center(location)
+                .zoom(14.0)
+                .build()
+        )
     }
 
     override fun onStop() {
         super.onStop()
-        binding.layoutMetric.root.visibility = View.VISIBLE
+        binding.icArrowDown.visibility = View.GONE
     }
 }
