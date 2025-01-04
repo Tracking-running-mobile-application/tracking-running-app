c5214448eea26c433c68aa298623e15ea960371d
thaison1939 <70836333+thaison1939@users.noreply.github.com>
2025-01-04 13:08:40 +0700

    Remove ImagePath from TrainingPlan

--- app/src/main/assets/database/initial_data.sql
+++ app/src/main/assets/database/initial_data.sql
@@ -10,7 +10,6 @@ CREATE TABLE IF NOT EXISTS "TrainingPlan" (
 	"targetCaloriesBurned"	REAL DEFAULT 0.0,
 	"goalProgress"	REAL DEFAULT 0.0,
 	"exerciseType"	TEXT NOT NULL,
-	"imagePath"	TEXT DEFAULT NULL,
 	"difficulty"	TEXT NOT NULL,
 	"lastRecommendedDate"	TEXT DEFAULT NULL,
 	"isFinished"	INTEGER DEFAULT 0 CHECK("isFinished" IN (0, 1)),
@@ -99,36 +98,36 @@ CREATE TABLE IF NOT EXISTS "YearlyStats" (
 	"totalAvgPace"	REAL DEFAULT 0.0,
 	PRIMARY KEY("yearlyStatsKey")
 );
-INSERT INTO "TrainingPlan" VALUES (1,NULL,'Running for Beginners','This plan helps new runners build a solid foundation of endurance and stamina while gradually increasing their distances.',30.0,5.0,NULL,NULL,0.0,'Running','app/src/main/res/drawable/img01.jpg','Beginner',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (2,NULL,'Trail Hiking Basics','Perfect for those starting their hiking journey, focusing on short distances and light trails to improve overall fitness.',60.0,2.0,NULL,NULL,0.0,'Hiking','app/src/main/res/drawable/img02.jpg','Beginner',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (3,NULL,'Mindful Movement','Combine light running with mindfulness exercises to improve focus and reduce stress while staying active.',20.0,NULL,20.0,NULL,0.0,'Mindful Running','app/src/main/res/drawable/img03.jpg','Beginner',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (4,NULL,'Strength Training for Trails','This plan introduces basic strength exercises to help trail runners navigate uneven terrains safely.',45.0,NULL,NULL,200.0,0.0,'Trail Running','app/src/main/res/drawable/img04.jpg','Beginner',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (5,NULL,'Trail Exploration Start','Begin exploring trails with manageable distances to build your endurance and adapt to new surfaces.',70.0,10.0,NULL,NULL,0.0,'Trail Running','app/src/main/res/drawable/img05.jpg','Beginner',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (6,NULL,'Easy Running Plan','Develop a consistent running habit by aligning breath and stride for better endurance and mental clarity.',30.0,NULL,30.0,NULL,0.0,'Running','app/src/main/res/drawable/img06.jpg','Beginner',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (7,NULL,'Hiking Preparation','Build the necessary stamina and strength for day-long hiking adventures with a focus on light trails.',90.0,3.0,NULL,NULL,0.0,'Hiking','app/src/main/res/drawable/img07.jpg','Beginner',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (8,NULL,'Introduction to Running','A beginner-friendly approach to running, focusing on consistency and gradual progression to cover moderate distances.',35.0,5.0,NULL,NULL,0.0,'Running','app/src/main/res/drawable/img08.jpg','Beginner',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (9,NULL,'Trail Basics','Learn the essentials of trail running, including handling varied terrains and maintaining proper balance.',50.0,6.0,NULL,NULL,0.0,'Trail Running','app/src/main/res/drawable/img09.jpg','Beginner',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (10,NULL,'Mindful Jogging','Focus on syncing your breathing with movement to enhance your running efficiency and mental focus.',15.0,NULL,15.0,NULL,0.0,'Mindful Running','app/src/main/res/drawable/img10.jpg','Beginner',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (11,NULL,'Distance Builder','Enhance your endurance through structured runs aimed at gradually increasing the distance you can cover.',60.0,10.0,NULL,NULL,0.0,'Running','app/src/main/res/drawable/img11.jpg','Intermediate',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (12,NULL,'Trail Endurance Plan','Prepare for trails with significant elevation changes through targeted exercises to build stamina and strength.',75.0,NULL,NULL,400.0,0.0,'Trail Running','app/src/main/res/drawable/img12.jpg','Intermediate',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (13,NULL,'Stamina for Long Runs','Focus on long, steady runs to improve endurance and prepare for covering greater distances.',90.0,15.0,NULL,NULL,0.0,'Running','app/src/main/res/drawable/img13.jpg','Intermediate',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (14,NULL,'Trail Fitness Plan','Challenge yourself with steady trail runs to improve your stamina and overall trail performance.',80.0,12.0,NULL,NULL,0.0,'Trail Running','app/src/main/res/drawable/img14.jpg','Intermediate',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (15,NULL,'Long Distance Focus','Work on pacing and energy management for longer durations, perfect for runners aiming to push their limits.',45.0,NULL,45.0,NULL,0.0,'Running','app/src/main/res/drawable/img15.jpg','Intermediate',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (16,NULL,'Mindful Endurance','Practice mindful techniques while running to help you stay focused and reduce fatigue during longer runs.',30.0,NULL,30.0,NULL,0.0,'Mindful Running','app/src/main/res/drawable/img16.jpg','Intermediate',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (17,NULL,'Advanced Trail Exploration','Master trail techniques like handling steep inclines and sharp descents to improve trail running performance.',100.0,10.0,NULL,NULL,0.0,'Trail Running','app/src/main/res/drawable/img17.jpg','Intermediate',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (18,NULL,'Trail Climbing Strength','Boost your strength with targeted hill climbing exercises designed for tackling trails with varying elevations.',90.0,NULL,NULL,450.0,0.0,'Trail Running','app/src/main/res/drawable/img18.jpg','Intermediate',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (19,NULL,'Focused Running','Combine mindful exercises with your runs to build focus and reduce fatigue, enhancing your overall running experience.',40.0,NULL,40.0,NULL,0.0,'Mindful Running','app/src/main/res/drawable/img19.jpg','Intermediate',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (20,NULL,'Trail Endurance Challenge','Push your trail fitness with runs that test your endurance and help you adapt to challenging surfaces.',85.0,12.0,NULL,NULL,0.0,'Trail Running','app/src/main/res/drawable/img20.jpg','Intermediate',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (21,NULL,'Advanced Distance Training','Prepare for advanced distances with structured long runs, speed intervals, and expert pacing strategies.',120.0,21.1,NULL,NULL,0.0,'Running','app/src/main/res/drawable/img21.jpg','Advanced',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (22,NULL,'Marathon Preparation','Develop the endurance needed for marathons with a plan focused on gradual distance increases and recovery.',240.0,42.2,NULL,NULL,0.0,'Running','app/src/main/res/drawable/img22.jpg','Advanced',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (23,NULL,'Trail Mastery','Learn advanced trail techniques to handle technical trails with steep descents, sharp turns, and rough surfaces.',150.0,20.0,NULL,NULL,0.0,'Trail Running','app/src/main/res/drawable/img23.jpg','Advanced',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (24,NULL,'Endurance Running','Enhance your stamina for ultra-distance events with a mix of long runs and focused interval training.',180.0,50.0,NULL,NULL,0.0,'Running','app/src/main/res/drawable/img24.jpg','Advanced',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (25,NULL,'Mindful Ultra Training','Strengthen your mental focus and reduce fatigue during ultra-distance runs by practicing advanced mindfulness techniques.',90.0,NULL,90.0,NULL,0.0,'Mindful Running','app/src/main/res/drawable/img25.jpg','Advanced',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (26,NULL,'Trail Marathon Preparation','Train for long trail marathons with a focus on endurance, pacing, and handling technical terrains.',200.0,42.2,NULL,NULL,0.0,'Trail Running','app/src/main/res/drawable/img26.jpg','Advanced',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (27,NULL,'Elite Hiking Endurance','Prepare for multi-day hiking adventures with targeted exercises to build strength and long-lasting endurance.',240.0,20.0,NULL,NULL,0.0,'Hiking','app/src/main/res/drawable/img27.jpg','Advanced',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (28,NULL,'Marathon Excellence','Focus on advanced strategies and training to achieve peak performance in competitive marathon events.',240.0,42.2,NULL,NULL,0.0,'Running','app/src/main/res/drawable/img28.jpg','Advanced',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (29,NULL,'Trail Climbing Mastery','Master steep climbs and descents with advanced training techniques to boost power and adaptability.',180.0,30.0,NULL,NULL,0.0,'Trail Running','app/src/main/res/drawable/img29.jpg','Advanced',NULL,0);
-INSERT INTO "TrainingPlan" VALUES (30,NULL,'Ultra Running Focus','Develop both the physical and mental endurance needed for ultra-distance running events with this advanced training.',300.0,NULL,120.0,NULL,0.0,'Mindful Running','app/src/main/res/drawable/img30.jpg','Advanced',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (1,NULL,'Running for Beginners','This plan helps new runners build a solid foundation of endurance and stamina while gradually increasing their distances.',30.0,5.0,NULL,NULL,0.0,'Running','Beginner',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (2,NULL,'Trail Hiking Basics','Perfect for those starting their hiking journey, focusing on short distances and light trails to improve overall fitness.',60.0,2.0,NULL,NULL,0.0,'Hiking','Beginner',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (3,NULL,'Mindful Movement','Combine light running with mindfulness exercises to improve focus and reduce stress while staying active.',20.0,NULL,20.0,NULL,0.0,'Mindful Running','Beginner',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (4,NULL,'Strength Training for Trails','This plan introduces basic strength exercises to help trail runners navigate uneven terrains safely.',45.0,NULL,NULL,200.0,0.0,'Trail Running','Beginner',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (5,NULL,'Trail Exploration Start','Begin exploring trails with manageable distances to build your endurance and adapt to new surfaces.',70.0,10.0,NULL,NULL,0.0,'Trail Running','Beginner',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (6,NULL,'Easy Running Plan','Develop a consistent running habit by aligning breath and stride for better endurance and mental clarity.',30.0,NULL,30.0,NULL,0.0,'Running','Beginner',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (7,NULL,'Hiking Preparation','Build the necessary stamina and strength for day-long hiking adventures with a focus on light trails.',90.0,3.0,NULL,NULL,0.0,'Hiking','Beginner',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (8,NULL,'Introduction to Running','A beginner-friendly approach to running, focusing on consistency and gradual progression to cover moderate distances.',35.0,5.0,NULL,NULL,0.0,'Running','Beginner',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (9,NULL,'Trail Basics','Learn the essentials of trail running, including handling varied terrains and maintaining proper balance.',50.0,6.0,NULL,NULL,0.0,'Trail Running','Beginner',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (10,NULL,'Mindful Jogging','Focus on syncing your breathing with movement to enhance your running efficiency and mental focus.',15.0,NULL,15.0,NULL,0.0,'Mindful Running','Beginner',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (11,NULL,'Distance Builder','Enhance your endurance through structured runs aimed at gradually increasing the distance you can cover.',60.0,10.0,NULL,NULL,0.0,'Running','Intermediate',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (12,NULL,'Trail Endurance Plan','Prepare for trails with significant elevation changes through targeted exercises to build stamina and strength.',75.0,NULL,NULL,400.0,0.0,'Trail Running','Intermediate',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (13,NULL,'Stamina for Long Runs','Focus on long, steady runs to improve endurance and prepare for covering greater distances.',90.0,15.0,NULL,NULL,0.0,'Running','Intermediate',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (14,NULL,'Trail Fitness Plan','Challenge yourself with steady trail runs to improve your stamina and overall trail performance.',80.0,12.0,NULL,NULL,0.0,'Trail Running','Intermediate',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (15,NULL,'Long Distance Focus','Work on pacing and energy management for longer durations, perfect for runners aiming to push their limits.',45.0,NULL,45.0,NULL,0.0,'Running','Intermediate',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (16,NULL,'Mindful Endurance','Practice mindful techniques while running to help you stay focused and reduce fatigue during longer runs.',30.0,NULL,30.0,NULL,0.0,'Mindful Running','Intermediate',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (17,NULL,'Advanced Trail Exploration','Master trail techniques like handling steep inclines and sharp descents to improve trail running performance.',100.0,10.0,NULL,NULL,0.0,'Trail Running','Intermediate',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (18,NULL,'Trail Climbing Strength','Boost your strength with targeted hill climbing exercises designed for tackling trails with varying elevations.',90.0,NULL,NULL,450.0,0.0,'Trail Running','Intermediate',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (19,NULL,'Focused Running','Combine mindful exercises with your runs to build focus and reduce fatigue, enhancing your overall running experience.',40.0,NULL,40.0,NULL,0.0,'Mindful Running','Intermediate',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (20,NULL,'Trail Endurance Challenge','Push your trail fitness with runs that test your endurance and help you adapt to challenging surfaces.',85.0,12.0,NULL,NULL,0.0,'Trail Running','Intermediate',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (21,NULL,'Advanced Distance Training','Prepare for advanced distances with structured long runs, speed intervals, and expert pacing strategies.',120.0,21.1,NULL,NULL,0.0,'Running','Advanced',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (22,NULL,'Marathon Preparation','Develop the endurance needed for marathons with a plan focused on gradual distance increases and recovery.',240.0,42.2,NULL,NULL,0.0,'Running','Advanced',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (23,NULL,'Trail Mastery','Learn advanced trail techniques to handle technical trails with steep descents, sharp turns, and rough surfaces.',150.0,20.0,NULL,NULL,0.0,'Trail Running','Advanced',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (24,NULL,'Endurance Running','Enhance your stamina for ultra-distance events with a mix of long runs and focused interval training.',180.0,50.0,NULL,NULL,0.0,'Running','Advanced',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (25,NULL,'Mindful Ultra Training','Strengthen your mental focus and reduce fatigue during ultra-distance runs by practicing advanced mindfulness techniques.',90.0,NULL,90.0,NULL,0.0,'Mindful Running','Advanced',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (26,NULL,'Trail Marathon Preparation','Train for long trail marathons with a focus on endurance, pacing, and handling technical terrains.',200.0,42.2,NULL,NULL,0.0,'Trail Running','Advanced',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (27,NULL,'Elite Hiking Endurance','Prepare for multi-day hiking adventures with targeted exercises to build strength and long-lasting endurance.',240.0,20.0,NULL,NULL,0.0,'Hiking','Advanced',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (28,NULL,'Marathon Excellence','Focus on advanced strategies and training to achieve peak performance in competitive marathon events.',240.0,42.2,NULL,NULL,0.0,'Running','Advanced',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (29,NULL,'Trail Climbing Mastery','Master steep climbs and descents with advanced training techniques to boost power and adaptability.',180.0,30.0,NULL,NULL,0.0,'Trail Running','Advanced',NULL,0);
+INSERT INTO "TrainingPlan" VALUES (30,NULL,'Ultra Running Focus','Develop both the physical and mental endurance needed for ultra-distance running events with this advanced training.',300.0,NULL,120.0,NULL,0.0,'Mindful Running','Advanced',NULL,0);
 INSERT INTO "RunSession" VALUES (1,'20241123',5.5,1800,5.8,350.0,0,NULL,0);
 INSERT INTO "RunSession" VALUES (2,'20241124',6.0,2000,5.7,400.0,0,NULL,0);
 INSERT INTO "RunSession" VALUES (3,'20241125',7.0,2300,5.5,450.0,0,NULL,0);
@@ -155,36 +154,11 @@ INSERT INTO "RunSession" VALUES (23,'20241214',7.3,2500,5.4,500.0,0,NULL,0);
 INSERT INTO "RunSession" VALUES (24,'20241216',6.0,2100,5.7,400.0,0,NULL,1);
 INSERT INTO "RunSession" VALUES (25,'20241217',5.3,1900,6.0,350.0,0,NULL,0);
 INSERT INTO "RunSession" VALUES (26,'20241218',7.0,2400,5.5,470.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (27,'20251219',5.8,2000,5.8,380.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (28,'20251221',6.4,2200,5.6,420.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (29,'20251221',4.7,1600,6.3,300.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (30,'20251221',2.7,1100,3.3,400.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (31,'20251222',5.1,1800,6.1,320.0,0,NULL,1);
-INSERT INTO "RunSession" VALUES (32,'20250105',4.2,1600,6.0,290.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (33,'20250215',5.7,2000,6.3,330.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (34,'20250308',3.9,1500,5.8,280.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (35,'20250412',6.5,2200,6.4,360.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (36,'20250522',4.8,1800,6.1,310.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (37,'20250610',7.1,2500,6.6,400.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (38,'20250704',3.4,1400,5.7,260.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (39,'20250816',5.9,2100,6.3,350.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (40,'20250909',4.0,1700,6.2,300.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (41,'20251025',6.8,2300,6.5,370.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (42,'20251113',4.3,1600,6.0,290.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (43,'20251207',7.4,2600,6.7,410.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (44,'20250102',3.7,1500,5.9,280.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (45,'20250218',6.2,2200,6.4,360.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (46,'20250311',4.9,1900,6.1,320.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (47,'20250424',5.5,2000,6.2,340.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (48,'20250515',7.3,2500,6.6,400.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (49,'20250630',4.1,1700,6.0,290.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (50,'20250719',6.0,2100,6.3,350.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (51,'20250814',3.8,1600,6.0,290.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (52,'20250905',6.4,2200,6.4,360.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (53,'20251021',5.1,2000,6.2,340.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (54,'20251117',4.6,1800,6.1,310.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (55,'20251209',7.0,2400,6.5,380.0,0,NULL,0);
-INSERT INTO "RunSession" VALUES (56,'20251228',3.5,1500,5.9,280.0,0,NULL,0);
+INSERT INTO "RunSession" VALUES (27,'20241219',5.8,2000,5.8,380.0,0,NULL,0);
+INSERT INTO "RunSession" VALUES (28,'20241221',6.4,2200,5.6,420.0,0,NULL,0);
+INSERT INTO "RunSession" VALUES (29,'20241221',4.7,1600,6.3,300.0,0,NULL,0);
+INSERT INTO "RunSession" VALUES (30,'20241221',2.7,1100,3.3,400.0,0,NULL,0);
+INSERT INTO "RunSession" VALUES (31,'20241222',5.1,1800,6.1,320.0,0,NULL,1);
 INSERT INTO "Notification" VALUES (1,'Halfway There!','You are halfway through your goal. Keep going!','HALF');
 INSERT INTO "Notification" VALUES (2,'Keep Pushing!','Halfway to your goal. Stay strong!','HALF');
 INSERT INTO "Notification" VALUES (3,'Midway Success!','You’re halfway done. Finish strong!','HALF');
@@ -215,7 +189,6 @@ INSERT INTO "Notification" VALUES (27,'Recharge!','Take a moment to rest and rej
 INSERT INTO "Notification" VALUES (28,'Breathe!','Catch your breath. It’s break time.','BREAK');
 INSERT INTO "Notification" VALUES (29,'Refresh!','Time to relax. You’ve earned it.','BREAK');
 INSERT INTO "Notification" VALUES (30,'Recover!','Take a well-deserved break and get ready to go again.','BREAK');
-INSERT INTO "Notification" VALUES (31,'Caution!','Your speed is unusually high! Proceed at your own risk!','EXCESSIVE_PACE');
 CREATE INDEX IF NOT EXISTS "index_GPSPoint_trackId" ON "GPSPoint" (
 	"trackId"
 );
