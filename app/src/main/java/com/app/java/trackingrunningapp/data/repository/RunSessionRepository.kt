1e869293ee498f83f378d30cb237f8b4049c903e
thaison1939 <70836333+thaison1939@users.noreply.github.com>
2025-01-21 17:18:50 +0700

    Fix calories burned formula

--- app/src/main/java/com/app/java/trackingrunningapp/data/repository/RunSessionRepository.kt
+++ app/src/main/java/com/app/java/trackingrunningapp/data/repository/RunSessionRepository.kt
@@ -56,6 +56,8 @@ class RunSessionRepository {
     private val _distance = MutableStateFlow<Double>(0.0)
     val distance: StateFlow<Double> = _distance
 
+    private var totalCaloriesBurned = 0.0
+
     private lateinit var runSessionStartTime: Instant
 
     private var repoScope = CoroutineScope(Job() + Dispatchers.IO)
@@ -87,6 +89,7 @@ class RunSessionRepository {
         _distance.value = 0.0
         _pace.value = 0.0
         _caloriesBurned.value = 0.0
+        totalCaloriesBurned = 0.0
         cumulativeDurationSeconds = 0L
         totalDurationSeconds = 0L
         durationNotification = false
@@ -190,23 +193,23 @@ class RunSessionRepository {
         ensureRepoScope()
         repoScope.launch {
             try {
-                val userUnitPreference = userInfo?.unit
+                val userMetricPreference = userInfo?.metricPreference
 
-                val durationInMinutes = _duration.value.div(60.0)
-                val adjustedDistance: Double = when (userUnitPreference) {
+                val durationInHours = _duration.value.div(3600.0)
+                val adjustedDistance: Double = when (userMetricPreference) {
                     User.UNIT_MILE -> _distance.value.times(0.621371)
                     else -> _distance.value
                 }
 
                 val pace: Double = if (adjustedDistance > 0) {
-                    adjustedDistance.div(durationInMinutes)
+                    adjustedDistance.div(durationInHours)
                 } else {
                     0.0
                 }
 
-                val excessivePace = when (userUnitPreference) {
-                    User.UNIT_MILE -> pace > 30
-                    else -> pace > 45
+                val excessivePace = when (userMetricPreference) {
+                    User.UNIT_MILE -> pace > 30.0
+                    else -> pace > 45.0
                 }
 
                 if (excessivePace && !paceNotification) {
@@ -229,36 +232,44 @@ class RunSessionRepository {
                 val userMetricPreference: String? = userInfo?.metricPreference
                 val unit: String? = userInfo?.unit
 
-                //val isTooSlow = when (userMetricPreference) {
-                //    User.UNIT_MILE -> _pace.value > 20.0
-                //    else -> _pace.value < 3.0
-                //}
+                val isTooSlow = when (userMetricPreference) {
+                    User.UNIT_MILE -> _pace.value < 3.5
+                    else -> _pace.value < 4.0
+                }
 
-                //if(isTooSlow) {
-                //    return@launch
-                //}
+                if(isTooSlow) {
+                    return@launch
+                }
 
-                val adjustedWeight = when (userMetricPreference) {
+                val adjustedWeight = when (unit) {
                     User.POUNDS -> userInfo?.weight?.times(0.45359237) ?: (50.0 * 0.45359237)
                     else -> userInfo?.weight ?: 50.0
                 }
 
-                val adjustedPace: Double = if (unit == User.UNIT_MILE)
-                    _pace.value else _pace.value * 1.609344
-
-                val speedMetersPerSec: Double = if (adjustedPace > 0) {
-                    1000.0 / (adjustedPace * 60.0)
-                } else {
-                    0.0
-                }
-
                 val durationInHours = _duration.value.div(3600.0)
 
-                val MET = when {
-                    speedMetersPerSec == 0.0 -> 0.0
-                    speedMetersPerSec < 2.0 -> 0.5 + (speedMetersPerSec * 0.3)
-                    speedMetersPerSec >= 2.0 -> 1.0 + (speedMetersPerSec * 0.9)
-                    else -> 0.0
+                val MET = if (userMetricPreference == User.UNIT_KM) {
+                    when {
+                        _pace.value == 0.0 -> 0.0
+                        _pace.value < 6.4 -> 2.0
+                        _pace.value in 6.4..8.0 -> 3.9
+                        _pace.value in 8.0..10.8 -> 6.0
+                        _pace.value in 10.8..12.9 -> 8.0
+                        _pace.value in 12.9..14.5 -> 10.0
+                        _pace.value in 14.5..16.1 -> 11.0
+                        else -> 12.0
+                    }
+                } else {
+                    when {
+                        _pace.value == 0.0 -> 0.0
+                        _pace.value < 4.0 -> 2.0
+                        _pace.value in 4.0..5.0 -> 3.9
+                        _pace.value in 5.0..6.7 -> 6.0
+                        _pace.value in 6.7..8.0 -> 8.0
+                        _pace.value in 8.0..9.0 -> 10.0
+                        _pace.value in 9.0..10.0 -> 11.0
+                        else -> 12.0
+                    }
                 }
 
                 val caloriesBurnedPerHour = MET * adjustedWeight
@@ -268,7 +279,9 @@ class RunSessionRepository {
                 } else {
                     0.0
                 }
-                _caloriesBurned.value = caloriesBurned
+
+                totalCaloriesBurned += caloriesBurned
+                _caloriesBurned.value = totalCaloriesBurned
 
                 Log.d("Calories", "${caloriesBurned}")
 
@@ -316,14 +329,6 @@ class RunSessionRepository {
                         Log.d("calc distance", "location 1: $location1, location 2: $location2")
 
                         val userUnitPreference = userInfo?.metricPreference
-                        //val isTooSlow = when (userUnitPreference) {
-                        //    User.UNIT_MILE -> _pace.value < 20.0
-                        //    else -> _pace.value < 4.0
-                        //}
-
-                        //if (isTooSlow) {
-                        //    return@collect
-                        //}
 
                         val distance = when (userUnitPreference) {
                             User.UNIT_MILE -> StatsUtils.haversineFormula(location1, location2) / 1609.34
