a9c3146b16e6c3ab0829ce6d95af56b72e93ace4
Kwang1402 <hongquang2k4@gmail.com>
2025-01-23 22:09:24 +0700

    Draw running route after session

--- app/src/main/java/com/app/java/trackingrunningapp/ui/history/DetailRunFragment.kt
+++ app/src/main/java/com/app/java/trackingrunningapp/ui/history/DetailRunFragment.kt
@@ -1,31 +1,61 @@
 package com.app.java.trackingrunningapp.ui.history
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.app.java.trackingrunningapp.R
 import com.app.java.trackingrunningapp.data.database.InitDatabase
 import com.app.java.trackingrunningapp.data.model.entity.User
+import com.app.java.trackingrunningapp.data.repository.RunSessionRepository
 import com.app.java.trackingrunningapp.databinding.FragmentDetailRunBinding
+import com.app.java.trackingrunningapp.ui.run.RunResultFragment.Companion.EXTRA_RUN_ID_RESULT
+import com.app.java.trackingrunningapp.ui.viewmodel.GPSTrackViewModel
+import com.app.java.trackingrunningapp.ui.viewmodel.GPSTrackViewModelFactory
 import com.app.java.trackingrunningapp.ui.viewmodel.RunSessionViewModel
 import com.app.java.trackingrunningapp.ui.viewmodel.RunSessionViewModelFactory
 import com.app.java.trackingrunningapp.ui.viewmodel.UserViewModel
 import com.app.java.trackingrunningapp.ui.viewmodel.UserViewModelFactory
 import com.app.java.trackingrunningapp.utils.DateTimeUtils
 import com.app.java.trackingrunningapp.utils.StatsUtils
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.annotation.AnnotationPlugin
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PolylineAnnotationManager
+import com.mapbox.maps.plugin.annotation.generated.PolylineAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPolylineAnnotationManager
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
+import com.mapbox.maps.plugin.locationcomponent.location
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class DetailRunFragment : Fragment() {
     private lateinit var binding: FragmentDetailRunBinding
     private lateinit var runSessionViewModel: RunSessionViewModel
     private lateinit var userViewModel: UserViewModel
+    private lateinit var gpsTrackViewModel: GPSTrackViewModel
+    private lateinit var mapView: MapView
+    private val routeCoordinates = mutableListOf<Point>()
+    private lateinit var annotationApi: AnnotationPlugin
+    private lateinit var polylineAnnotationManager: PolylineAnnotationManager
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        val gpsTrackFactory = GPSTrackViewModelFactory(InitDatabase.gpsTrackRepository)
+        gpsTrackViewModel =
+            ViewModelProvider(this, gpsTrackFactory).get(GPSTrackViewModel::class.java)
         binding = FragmentDetailRunBinding.inflate(inflater, container, false)
         val runFactory = RunSessionViewModelFactory(InitDatabase.runSessionRepository)
         runSessionViewModel =
@@ -63,6 +93,63 @@ class DetailRunFragment : Fragment() {
                 }
             }
         }
+        initMap()
+    }
+
+    private fun initMap() {
+        mapView = binding.mapView
+        annotationApi = mapView.annotations
+        polylineAnnotationManager = annotationApi.createPolylineAnnotationManager()
+        val runId = arguments?.getInt(EXTRA_HISTORY_RUN_ID, 0) ?: 0
+        Log.d("RunID", runId.toString())
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                val locations =  gpsTrackViewModel.fetchGPSPoints(runId)
+                for (location in locations) {
+                    routeCoordinates.add(Point.fromLngLat(location.longitude, location.latitude))
+                }
+            }
+            drawRoute()
+        }
+
+        lateinit var targetPoint: Point
+        mapView.mapboxMap.loadStyle(Style.STANDARD) {
+            val locationComponentPlugin = mapView.location
+            locationComponentPlugin.updateSettings {
+                this.enabled = true
+            }
+            mapView.location.locationPuck = LocationPuck2D(
+                topImage = null,
+                bearingImage = null,
+                shadowImage = null
+            )
+            locationComponentPlugin.addOnIndicatorPositionChangedListener(object :
+                OnIndicatorPositionChangedListener {
+                override fun onIndicatorPositionChanged(point: Point) {
+                    targetPoint = point
+                    if (routeCoordinates.isNotEmpty()) {
+                        targetPoint = routeCoordinates.last()
+                    }
+                    mapView.mapboxMap.setCamera(
+                        CameraOptions.Builder()
+                            .center(targetPoint)
+                            .zoom(15.0)
+                            .build()
+                    )
+
+                    locationComponentPlugin.removeOnIndicatorPositionChangedListener(this)
+                }
+            })
+        }
+    }
+
+    private fun drawRoute() {
+        polylineAnnotationManager.deleteAll()
+        val polylineAnnotationOptions: PolylineAnnotationOptions = PolylineAnnotationOptions()
+            .withPoints(routeCoordinates)
+            .withLineColor("#FF0000")
+            .withLineWidth(5.0)
+        polylineAnnotationManager.create(polylineAnnotationOptions)
     }
 
     companion object {
